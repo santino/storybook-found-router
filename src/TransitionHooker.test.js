a250255b33cde02ba1872fa9b801dda13d380fb3
Quentin Gérome <q.gerome@whatever-company.com>
2020-04-07 16:04:44 +0200

    chore: run standard to respect the code styleguide

--- src/TransitionHooker.test.js
+++ src/TransitionHooker.test.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { mount, shallow } from "enzyme";
-import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
-import TransitionHooker from "./TransitionHooker";
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+import TransitionHooker from './TransitionHooker'
 
-const mockActionAddon = jest.fn();
-const mockLinkAddon = jest.fn();
-jest.mock("@storybook/addon-actions", () => ({
-  action: jest.fn(() => mockActionAddon),
-}));
-jest.mock("@storybook/addon-links", () => ({
-  linkTo: jest.fn(() => mockLinkAddon),
-}));
+const mockActionAddon = jest.fn()
+const mockLinkAddon = jest.fn()
+jest.mock('@storybook/addon-actions', () => ({
+  action: jest.fn(() => mockActionAddon)
+}))
+jest.mock('@storybook/addon-links', () => ({
+  linkTo: jest.fn(() => mockLinkAddon)
+}))
 
 global.console = {
   info: jest.fn(),
-  warn: jest.fn(),
-};
+  warn: jest.fn()
+}
 
-const storyFn = () => <div>my story</div>;
+const storyFn = () => <div>my story</div>
 
-describe("TransitionHooker", () => {
+describe('TransitionHooker', () => {
   const routerProp = {
     router: {
       push: jest.fn(),
@@ -33,236 +33,236 @@ describe("TransitionHooker", () => {
         match: jest.fn(),
         getRoutes: jest.fn(),
         isActive: jest.fn(),
-        format: jest.fn(),
+        format: jest.fn()
       },
-      addNavigationListener: jest.fn(),
-    },
-  };
+      addNavigationListener: jest.fn()
+    }
+  }
   const wrap = () =>
-    mount(<TransitionHooker {...routerProp} story={storyFn} />);
+    mount(<TransitionHooker {...routerProp} story={storyFn} />)
 
-  describe("React Component", () => {
+  describe('React Component', () => {
     afterEach(() => {
-      routerProp.router.addNavigationListener.mockClear();
-    });
+      routerProp.router.addNavigationListener.mockClear()
+    })
 
-    it("renders the passed story element as a children", () => {
-      const wrapper = wrap();
+    it('renders the passed story element as a children', () => {
+      const wrapper = wrap()
 
-      expect(wrapper.find("TransitionHooker")).toHaveLength(1);
-      expect(wrapper.children().html()).toEqual(shallow(storyFn()).html());
-    });
+      expect(wrapper.find('TransitionHooker')).toHaveLength(1)
+      expect(wrapper.children().html()).toEqual(shallow(storyFn()).html())
+    })
 
-    it("adds the internal onTransition method as a TransitionHook", () => {
-      const wrapper = wrap();
-      wrapper.instance().removeNavigationListener = jest.fn();
+    it('adds the internal onTransition method as a TransitionHook', () => {
+      const wrapper = wrap()
+      wrapper.instance().removeNavigationListener = jest.fn()
 
-      expect(routerProp.router.addNavigationListener).toHaveBeenCalledTimes(1);
+      expect(routerProp.router.addNavigationListener).toHaveBeenCalledTimes(1)
       expect(routerProp.router.addNavigationListener).toHaveBeenCalledWith(
         wrapper.instance().onTransition
-      );
-    });
+      )
+    })
 
-    it("sets and execute removeNavigationListener method on componentWillUnmount", () => {
-      const wrapper = wrap();
-      const mockRemoveNavigationListener = jest.fn();
+    it('sets and execute removeNavigationListener method on componentWillUnmount', () => {
+      const wrapper = wrap()
+      const mockRemoveNavigationListener = jest.fn()
 
-      wrapper.instance().removeNavigationListener = mockRemoveNavigationListener;
+      wrapper.instance().removeNavigationListener = mockRemoveNavigationListener
 
-      expect(wrapper.instance().componentWillUnmount).toBeDefined();
-      wrapper.unmount();
-      expect(mockRemoveNavigationListener).toHaveBeenCalled();
-    });
-  });
+      expect(wrapper.instance().componentWillUnmount).toBeDefined()
+      wrapper.unmount()
+      expect(mockRemoveNavigationListener).toHaveBeenCalled()
+    })
+  })
 
-  describe("onTransition method", () => {
+  describe('onTransition method', () => {
     afterEach(() => {
-      mockActionAddon.mockClear();
-      action.mockClear();
-      routerProp.router.matcher.match.mockClear();
-      routerProp.router.matcher.getRoutes.mockClear();
-    });
+      mockActionAddon.mockClear()
+      action.mockClear()
+      routerProp.router.matcher.match.mockClear()
+      routerProp.router.matcher.getRoutes.mockClear()
+    })
 
-    it("calls handleMatchingSuccess when matching route with story string", () => {
-      const wrapper = wrap();
+    it('calls handleMatchingSuccess when matching route with story string', () => {
+      const wrapper = wrap()
       routerProp.router.matcher.match.mockReturnValueOnce({
-        routeIndices: [0],
-      });
+        routeIndices: [0]
+      })
       routerProp.router.matcher.getRoutes.mockReturnValueOnce([
-        { story: "HomePage" },
-      ]);
-      wrapper.instance().handleMatchingFailure = jest.fn();
-      wrapper.instance().handleMatchingSuccess = jest.fn();
-      wrapper.instance().onTransition({ action: "PUSH", pathname: "/" });
+        { story: 'HomePage' }
+      ])
+      wrapper.instance().handleMatchingFailure = jest.fn()
+      wrapper.instance().handleMatchingSuccess = jest.fn()
+      wrapper.instance().onTransition({ action: 'PUSH', pathname: '/' })
 
-      expect(action).toHaveBeenCalledWith("PUSH");
-      expect(mockActionAddon).toHaveBeenCalledWith("/");
-      expect(wrapper.instance().handleMatchingFailure).not.toHaveBeenCalled();
+      expect(action).toHaveBeenCalledWith('PUSH')
+      expect(mockActionAddon).toHaveBeenCalledWith('/')
+      expect(wrapper.instance().handleMatchingFailure).not.toHaveBeenCalled()
       expect(wrapper.instance().handleMatchingSuccess).toHaveBeenCalledWith(
-        "HomePage",
-        "default"
-      );
-    });
+        'HomePage',
+        'default'
+      )
+    })
 
-    it("calls handleMatchingSuccess when matching route with story array of 2 elements", () => {
-      const wrapper = wrap();
+    it('calls handleMatchingSuccess when matching route with story array of 2 elements', () => {
+      const wrapper = wrap()
       routerProp.router.matcher.match.mockReturnValueOnce({
-        routeIndices: [0],
-      });
+        routeIndices: [0]
+      })
       routerProp.router.matcher.getRoutes.mockReturnValueOnce([
-        { story: ["LoginForm", "invalid"] },
-      ]);
-      wrapper.instance().handleMatchingFailure = jest.fn();
-      wrapper.instance().handleMatchingSuccess = jest.fn();
-      wrapper.instance().onTransition({ action: "PUSH", pathname: "/login" });
+        { story: ['LoginForm', 'invalid'] }
+      ])
+      wrapper.instance().handleMatchingFailure = jest.fn()
+      wrapper.instance().handleMatchingSuccess = jest.fn()
+      wrapper.instance().onTransition({ action: 'PUSH', pathname: '/login' })
 
-      expect(action).toHaveBeenCalledWith("PUSH");
-      expect(mockActionAddon).toHaveBeenCalledWith("/login");
-      expect(wrapper.instance().handleMatchingFailure).not.toHaveBeenCalled();
+      expect(action).toHaveBeenCalledWith('PUSH')
+      expect(mockActionAddon).toHaveBeenCalledWith('/login')
+      expect(wrapper.instance().handleMatchingFailure).not.toHaveBeenCalled()
       expect(wrapper.instance().handleMatchingSuccess).toHaveBeenCalledWith(
-        "LoginForm",
-        "invalid"
-      );
-    });
+        'LoginForm',
+        'invalid'
+      )
+    })
 
-    it("calls handleMatchingSuccess when matching route with story array ignoring extra elements", () => {
-      const wrapper = wrap();
+    it('calls handleMatchingSuccess when matching route with story array ignoring extra elements', () => {
+      const wrapper = wrap()
       routerProp.router.matcher.match.mockReturnValueOnce({
-        routeIndices: [0],
-      });
+        routeIndices: [0]
+      })
       routerProp.router.matcher.getRoutes.mockReturnValueOnce([
-        { story: ["LoginForm", "invalid", "foo", "bar"] },
-      ]);
-      wrapper.instance().handleMatchingFailure = jest.fn();
-      wrapper.instance().handleMatchingSuccess = jest.fn();
-      wrapper.instance().onTransition({ action: "PUSH", pathname: "/login" });
+        { story: ['LoginForm', 'invalid', 'foo', 'bar'] }
+      ])
+      wrapper.instance().handleMatchingFailure = jest.fn()
+      wrapper.instance().handleMatchingSuccess = jest.fn()
+      wrapper.instance().onTransition({ action: 'PUSH', pathname: '/login' })
 
-      expect(action).toHaveBeenCalledWith("PUSH");
-      expect(mockActionAddon).toHaveBeenCalledWith("/login");
-      expect(wrapper.instance().handleMatchingFailure).not.toHaveBeenCalled();
+      expect(action).toHaveBeenCalledWith('PUSH')
+      expect(mockActionAddon).toHaveBeenCalledWith('/login')
+      expect(wrapper.instance().handleMatchingFailure).not.toHaveBeenCalled()
       expect(wrapper.instance().handleMatchingSuccess).toHaveBeenCalledWith(
-        "LoginForm",
-        "invalid"
-      );
-    });
+        'LoginForm',
+        'invalid'
+      )
+    })
 
-    it("calls handleMatchingFailure when matching route without story", () => {
-      const wrapper = wrap();
+    it('calls handleMatchingFailure when matching route without story', () => {
+      const wrapper = wrap()
       routerProp.router.matcher.match.mockReturnValueOnce({
-        routeIndices: [0],
-      });
-      routerProp.router.matcher.getRoutes.mockReturnValueOnce([{}]);
-      wrapper.instance().handleMatchingFailure = jest.fn();
-      wrapper.instance().handleMatchingSuccess = jest.fn();
-      wrapper.instance().onTransition({ action: "PUSH", pathname: "/" });
+        routeIndices: [0]
+      })
+      routerProp.router.matcher.getRoutes.mockReturnValueOnce([{}])
+      wrapper.instance().handleMatchingFailure = jest.fn()
+      wrapper.instance().handleMatchingSuccess = jest.fn()
+      wrapper.instance().onTransition({ action: 'PUSH', pathname: '/' })
 
-      expect(action).toHaveBeenCalledWith("PUSH");
-      expect(mockActionAddon).toHaveBeenCalledWith("/");
-      expect(wrapper.instance().handleMatchingFailure).toHaveBeenCalledWith();
-      expect(wrapper.instance().handleMatchingSuccess).not.toHaveBeenCalled();
-    });
+      expect(action).toHaveBeenCalledWith('PUSH')
+      expect(mockActionAddon).toHaveBeenCalledWith('/')
+      expect(wrapper.instance().handleMatchingFailure).toHaveBeenCalledWith()
+      expect(wrapper.instance().handleMatchingSuccess).not.toHaveBeenCalled()
+    })
 
-    it("calls handleMatchingFailure when not matching route", () => {
-      const wrapper = wrap();
-      routerProp.router.matcher.match.mockReturnValueOnce(null);
-      wrapper.instance().handleMatchingFailure = jest.fn();
-      wrapper.instance().handleMatchingSuccess = jest.fn();
-      wrapper.instance().onTransition({ action: "PUSH", pathname: "/" });
+    it('calls handleMatchingFailure when not matching route', () => {
+      const wrapper = wrap()
+      routerProp.router.matcher.match.mockReturnValueOnce(null)
+      wrapper.instance().handleMatchingFailure = jest.fn()
+      wrapper.instance().handleMatchingSuccess = jest.fn()
+      wrapper.instance().onTransition({ action: 'PUSH', pathname: '/' })
 
-      expect(action).toHaveBeenCalledWith("PUSH");
-      expect(mockActionAddon).toHaveBeenCalledWith("/");
-      expect(routerProp.router.matcher.getRoutes).not.toHaveBeenCalled();
-      expect(wrapper.instance().handleMatchingFailure).toHaveBeenCalledWith();
-      expect(wrapper.instance().handleMatchingSuccess).not.toHaveBeenCalled();
-    });
+      expect(action).toHaveBeenCalledWith('PUSH')
+      expect(mockActionAddon).toHaveBeenCalledWith('/')
+      expect(routerProp.router.matcher.getRoutes).not.toHaveBeenCalled()
+      expect(wrapper.instance().handleMatchingFailure).toHaveBeenCalledWith()
+      expect(wrapper.instance().handleMatchingSuccess).not.toHaveBeenCalled()
+    })
 
-    it("traverses single empty object when present in routes array", () => {
-      const wrapper = wrap();
+    it('traverses single empty object when present in routes array', () => {
+      const wrapper = wrap()
       routerProp.router.matcher.match.mockReturnValueOnce({
-        routeIndices: [0],
-      });
+        routeIndices: [0]
+      })
       routerProp.router.matcher.getRoutes.mockReturnValueOnce([
-        { story: "foo" },
+        { story: 'foo' },
+        {}
+      ])
+      wrapper.instance().handleMatchingFailure = jest.fn()
+      wrapper.instance().handleMatchingSuccess = jest.fn()
+      wrapper.instance().onTransition({ action: 'PUSH', pathname: '/' })
+
+      expect(action).toHaveBeenCalledWith('PUSH')
+      expect(mockActionAddon).toHaveBeenCalledWith('/')
+      expect(wrapper.instance().handleMatchingFailure).not.toHaveBeenCalled()
+      expect(wrapper.instance().handleMatchingSuccess).toHaveBeenCalledWith(
+        'foo',
+        'default'
+      )
+    })
+
+    it('traverses multiple empty objects when present in routes array', () => {
+      const wrapper = wrap()
+      routerProp.router.matcher.match.mockReturnValueOnce({
+        routeIndices: [0]
+      })
+      routerProp.router.matcher.getRoutes.mockReturnValueOnce([
+        { story: 'foo' },
         {},
-      ]);
-      wrapper.instance().handleMatchingFailure = jest.fn();
-      wrapper.instance().handleMatchingSuccess = jest.fn();
-      wrapper.instance().onTransition({ action: "PUSH", pathname: "/" });
+        {},
+        {}
+      ])
+      wrapper.instance().handleMatchingFailure = jest.fn()
+      wrapper.instance().handleMatchingSuccess = jest.fn()
+      wrapper.instance().onTransition({ action: 'PUSH', pathname: '/' })
 
-      expect(action).toHaveBeenCalledWith("PUSH");
-      expect(mockActionAddon).toHaveBeenCalledWith("/");
-      expect(wrapper.instance().handleMatchingFailure).not.toHaveBeenCalled();
+      expect(action).toHaveBeenCalledWith('PUSH')
+      expect(mockActionAddon).toHaveBeenCalledWith('/')
+      expect(wrapper.instance().handleMatchingFailure).not.toHaveBeenCalled()
       expect(wrapper.instance().handleMatchingSuccess).toHaveBeenCalledWith(
-        "foo",
-        "default"
-      );
-    });
+        'foo',
+        'default'
+      )
+    })
+  })
 
-    it("traverses multiple empty objects when present in routes array", () => {
-      const wrapper = wrap();
-      routerProp.router.matcher.match.mockReturnValueOnce({
-        routeIndices: [0],
-      });
-      routerProp.router.matcher.getRoutes.mockReturnValueOnce([
-        { story: "foo" },
-        {},
-        {},
-        {},
-      ]);
-      wrapper.instance().handleMatchingFailure = jest.fn();
-      wrapper.instance().handleMatchingSuccess = jest.fn();
-      wrapper.instance().onTransition({ action: "PUSH", pathname: "/" });
-
-      expect(action).toHaveBeenCalledWith("PUSH");
-      expect(mockActionAddon).toHaveBeenCalledWith("/");
-      expect(wrapper.instance().handleMatchingFailure).not.toHaveBeenCalled();
-      expect(wrapper.instance().handleMatchingSuccess).toHaveBeenCalledWith(
-        "foo",
-        "default"
-      );
-    });
-  });
-
-  describe("handleMatchingFailure method", () => {
+  describe('handleMatchingFailure method', () => {
     afterEach(() => {
-      console.warn.mockClear();
-      mockLinkAddon.mockClear();
-      linkTo.mockClear();
-    });
+      console.warn.mockClear()
+      mockLinkAddon.mockClear()
+      linkTo.mockClear()
+    })
 
-    it("should log a warn message to notify that no route has been matched and return false", () => {
-      const wrapper = wrap();
-      const handleMatchingFailure = wrapper.instance().handleMatchingFailure();
+    it('should log a warn message to notify that no route has been matched and return false', () => {
+      const wrapper = wrap()
+      const handleMatchingFailure = wrapper.instance().handleMatchingFailure()
 
       expect(console.warn).toHaveBeenCalledWith(
-        "storybook-found-router:",
-        expect.stringContaining("No route matched")
-      );
-      expect(handleMatchingFailure).toBeFalsy();
-      expect(linkTo).not.toHaveBeenCalled();
-      expect(mockLinkAddon).not.toHaveBeenCalled();
-    });
-  });
+        'storybook-found-router:',
+        expect.stringContaining('No route matched')
+      )
+      expect(handleMatchingFailure).toBeFalsy()
+      expect(linkTo).not.toHaveBeenCalled()
+      expect(mockLinkAddon).not.toHaveBeenCalled()
+    })
+  })
 
-  describe("handleMatchingSuccess method", () => {
+  describe('handleMatchingSuccess method', () => {
     afterEach(() => {
-      console.info.mockClear();
-      mockLinkAddon.mockClear();
-      linkTo.mockClear();
-    });
+      console.info.mockClear()
+      mockLinkAddon.mockClear()
+      linkTo.mockClear()
+    })
 
-    it("should log an info message with Kind and Story and call addon linkTo", () => {
-      const wrapper = wrap();
-      const kind = "HomePage";
-      const story = "default";
-      wrapper.instance().handleMatchingSuccess(kind, story);
+    it('should log an info message with Kind and Story and call addon linkTo', () => {
+      const wrapper = wrap()
+      const kind = 'HomePage'
+      const story = 'default'
+      wrapper.instance().handleMatchingSuccess(kind, story)
 
       expect(console.info).toHaveBeenCalledWith(
-        "storybook-found-router:",
+        'storybook-found-router:',
         expect.stringContaining(`'${kind}', '${story}'`)
-      );
-      expect(linkTo).toHaveBeenCalledWith(kind, story);
-      expect(mockLinkAddon).toHaveBeenCalledWith();
-    });
-  });
-});
+      )
+      expect(linkTo).toHaveBeenCalledWith(kind, story)
+      expect(mockLinkAddon).toHaveBeenCalledWith()
+    })
+  })
+})
--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-import React from "react";
-import createFarceRouter from "found/createFarceRouter";
-import createRender from "found/createRender";
-import MemoryProtocol from "farce/MemoryProtocol";
-import resolver from "found/resolver";
-import TransitionHooker from "./TransitionHooker";
+import React from 'react'
+import createFarceRouter from 'found/createFarceRouter'
+import createRender from 'found/createRender'
+import MemoryProtocol from 'farce/MemoryProtocol'
+import resolver from 'found/resolver'
+import TransitionHooker from './TransitionHooker'
 
 // Due to a breaking change introduced in found 4.0 we need to
 // add empty objects to route children array, if not present.
 // As discussed in https://github.com/4Catalyzer/found/issues/657
 export const normaliseRouteChildren = (input) => {
-  let childrenHasEmptyArray = false;
+  let childrenHasEmptyArray = false
   const children =
     !!input.children &&
     input.children.map((current) => {
       childrenHasEmptyArray =
-        childrenHasEmptyArray || Object.keys(current).length === 0;
+        childrenHasEmptyArray || Object.keys(current).length === 0
 
-      return normaliseRouteChildren(current);
-    });
+      return normaliseRouteChildren(current)
+    })
 
   if (children && !childrenHasEmptyArray) {
-    children.unshift({});
+    children.unshift({})
   }
-  return { ...input, ...(children && { children }) };
-};
+  return { ...input, ...(children && { children }) }
+}
 
-const storyRouterDecorator = (routes = [{}], initialLocation = "") => {
+const storyRouterDecorator = (routes = [{}], initialLocation = '') => {
   const decorator = (story) => {
-    const rootPath = routes[0].path === "/" ? "" : "/";
+    const rootPath = routes[0].path === '/' ? '' : '/'
     const StoryRouter = createFarceRouter({
       historyProtocol: new MemoryProtocol(initialLocation),
       routeConfig: [
@@ -37,15 +37,15 @@ const storyRouterDecorator = (routes = [{}], initialLocation = "") => {
           render: ({ Component, props }) => (
             <Component {...props} story={story} />
           ),
-          children: routes.map(normaliseRouteChildren),
-        },
+          children: routes.map(normaliseRouteChildren)
+        }
       ],
-      render: createRender({}),
-    });
-    return <StoryRouter resolver={resolver} />;
-  };
-  decorator.displayName = "StoryRouter";
-  return decorator;
-};
+      render: createRender({})
+    })
+    return <StoryRouter resolver={resolver} />
+  }
+  decorator.displayName = 'StoryRouter'
+  return decorator
+}
 
-export default storyRouterDecorator;
+export default storyRouterDecorator
